468ba620aaf1943da3e9dfd9d7bd0c2e644f91e8
M-R-T-U-D <M.Rashad@student.tudelft.nl>
2022-06-08 01:29:52 +0200

    fixed bugs in sh file

--- deploy-kube.sh
+++ deploy-kube.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+# function used to wait for creation of a resource in kubernetes
+# wait_till_created_kube()
+# {
+#     RESOURCE=$1
+#     NAME=$2
+#     while ! minikube kubectl -- get $RESOURCE/$NAME -A; do sleep 1; done
+# }
+
+
+# function used to wait for a resource to be ready
+# wait_till_ready_kube()
+# {
+#     wait_till_created_kube $1 $2
+#     minikube kubectl -- wait --for=condition=ready -n cockroach-operator-system --all deployments
+# }
+
 minikube start
 
 eval $(minikube -p minikube docker-env)
@@ -16,25 +32,26 @@ minikube kubectl -- apply -f crds.yaml
 
 minikube kubectl -- apply -f operator.yaml
 
-sleep 50
+sleep 65
+# minikube kubectl -- wait --for=condition=ready -n cockroach-operator-system --all deployments
 
 minikube kubectl -- apply -f example.yaml
 
-sleep 60
+sleep 45
+# minikube kubectl -- wait --for=condition=ready -n default --all statefulsets
 
 minikube kubectl -- apply -f client-secure-operator.yaml
 
-sleep 40
+sleep 45
 
 cd ..
 
-minikube kubectl -- apply -f haproxyIngress.yaml
+# minikube kubectl -- apply -f haproxyIngress.yaml
 minikube kubectl -- apply -f ingress-service.yaml
 minikube kubectl -- apply -f order-app.yaml
 minikube kubectl -- apply -f payment-app.yaml
 minikube kubectl -- apply -f stock-app.yaml
 
-
 cd ..
 
-cat dbinit.sql | minikube kubectl -- exec -it --namespace=default cockroachdb-client-secure -- ./cockroach sql --certs-dir=/cockroach/cockroach-certs --host=cockroachdb-public
+cat dbinit.sql | minikube kubectl -- exec -i --namespace=default cockroachdb-client-secure -- ./cockroach sql --certs-dir=/cockroach/cockroach-certs --host=cockroachdb-public
